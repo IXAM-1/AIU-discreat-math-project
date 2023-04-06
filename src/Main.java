a0db951b8a45d93434add20a04183c44527aea62
Mohamed <asonson047@gmail.com>
2023-04-06 03:46:33 +0200

    added getAscii

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the message to be encrypted ");
+        System.out.print("Please enter two prime numbers ");
+        int Prime1 = input.nextInt();
+        int Prime2 = input.nextInt();
+        String message = input.next();
+ArrayList<Integer> AsciiList = getAscii(message);
+
+    }
+    //This method turns the string into a list of ascii numbers to encrypt them in another method
+    public static ArrayList <Integer> getAscii(String input){
+        ArrayList <Integer> CharacterList = new ArrayList<>(); // initialize the list
+        for (int i = 0;i<=input.length();i++){ // we use for loop to type cast the Characters into ascii and put them into the list
+            CharacterList.add((int) input.charAt(i));
+        }
+
+        return CharacterList;
     }
 
 }
